cdcd6f5da43281885ebd25f2ba167d1255f55769
partharoyy <69928055+partharoyy@users.noreply.github.com>
2024-09-30 23:18:08 +0530

    Complete create-workspace-modal

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const CreateWorkspaceModal = () => {
   const [open, setOpen] = useCreateWorkspaceModal();
@@ -17,6 +19,19 @@ const CreateWorkspaceModal = () => {
         <DialogHeader>
           <DialogTitle>Add a workspace</DialogTitle>
         </DialogHeader>
+        <form className='space-y-4'>
+          <Input
+            value=''
+            disabled={false}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+          />
+          <div className='flex justify-end'>
+            <Button disabled={false}>Create</Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
